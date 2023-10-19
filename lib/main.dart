4d314327aa3184bb8ea98a831ce8d3f416d40782
hp113 <hariprasad2315@gmail.com>
2023-10-19 16:12:19 +0530

    git ignore checking

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+//hello
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
